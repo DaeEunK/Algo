ae0a34ff65b1ed29e181c4eb72b722a24e2c6ca8
DaeEunK <134470252+DaeEunK@users.noreply.github.com>
2023-05-25 13:34:52 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 14.40 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int idx = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ' ') idx = 1;
+            if(idx % 2 == 0) answer += Character.toUpperCase(s.charAt(i));
+            else answer += Character.toLowerCase(s.charAt(i));
+            idx++;
+        }
+        
+        return answer;
+    }
+}
