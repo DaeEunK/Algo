263ccc628ec3b00159b2381ab8a82204e64ada43
DaeEunK <134470252+DaeEunK@users.noreply.github.com>
2024-04-04 09:45:21 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT A.CART_ID
+FROM (
+        SELECT CART_ID
+        FROM CART_PRODUCTS
+        WHERE NAME = 'Milk'
+     ) A, (
+        SELECT CART_ID
+        FROM CART_PRODUCTS
+        WHERE NAME = 'Yogurt'
+     ) B 
+WHERE A.CART_ID = B.CART_ID
+ORDER BY A.CART_ID
